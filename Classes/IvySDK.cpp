1301f81cd148c40b7e752679e3ecb30b2440d1cb
张东 <timorhack@gmail.com>
2017-09-18 11:42:47 +0800

    修复计费失败回调和获取价格回调bug

--- Classes/IvySDK.cpp
+++ Classes/IvySDK.cpp
@@ -18,7 +18,6 @@ namespace IvySDK {
 extern "C" {
 #endif
     JNIEXPORT void JNICALL Java_com_android_client_Cocos_pr(JNIEnv* env, jclass clazz, jint billingId, jint status) {
-        CCLOG("payment result");
         if (IvySDK::paymentCallback_ != nullptr) {
             IvySDK::paymentCallback_(status, billingId);
         }
@@ -29,20 +28,17 @@ extern "C" {
     }
     
     JNIEXPORT void JNICALL Java_com_android_client_Cocos_rr(JNIEnv* env, jclass clazz, jboolean success, jint rewardId) {
-        CCLOG("receive free coins");
         if (IvySDK::rewardAdCallback_ != nullptr)
             IvySDK::rewardAdCallback_(success, rewardId);
     }
     
     JNIEXPORT void JNICALL Java_com_android_client_Cocos_sns(JNIEnv* env, jclass clazz, jint msg, jboolean success, jint extra){
-        CCLOG("receive sns message: %d, result: %d", msg, success? 1 : 0);
         if (IvySDK::snsCallback_ != nullptr) {
             IvySDK::snsCallback_(msg, success, extra);
         }
     }
     
     JNIEXPORT void JNICALL Java_com_android_client_Cocos_lb(JNIEnv* env, jclass clazz, jboolean submit, jboolean success, jstring leaderBoardId, jstring extra){
-        CCLOG("receive leader board message: %s, result: %d", submit ? "submit" : "load", success? 1 : 0);
         if (IvySDK::leaderBoardCallback_ != nullptr) {
             const char* id = env->GetStringUTFChars(leaderBoardId, 0);
             env->DeleteLocalRef(leaderBoardId);
@@ -53,7 +49,6 @@ extern "C" {
     }
     
     JNIEXPORT void JNICALL Java_com_android_client_Cocos_sr(JNIEnv* env, jclass clazz, jint resultCode, jboolean success, jstring ex) {
-        CCLOG("receive server result: %d", resultCode);
         if (IvySDK::serverCallback_ != nullptr) {
             const char* extra = env->GetStringUTFChars(ex, 0);
             env->DeleteLocalRef(ex);
@@ -63,7 +58,6 @@ extern "C" {
     
     
     JNIEXPORT void JNICALL Java_com_android_client_Cocos_url(JNIEnv* env, jclass clazz, jint tag, jboolean success, jstring ex) {
-        CCLOG("receive url result: %d", tag);
         if (IvySDK::cacheCallback_ != nullptr) {
             const char* extra = env->GetStringUTFChars(ex, 0);
             env->DeleteLocalRef(ex);
@@ -72,7 +66,6 @@ extern "C" {
     }
 
 	JNIEXPORT void JNICALL Java_com_android_client_Cocos_awc(JNIEnv* env, jclass clazz, jint adtype, jstring tag) {
-		CCLOG("receive url result: %d", tag);
 		if (IvySDK::adclickedCallback_ != nullptr) {
 			const char* extra = env->GetStringUTFChars(tag, 0);
 			env->DeleteLocalRef(tag);
@@ -81,7 +74,6 @@ extern "C" {
 	}
 
 	JNIEXPORT void JNICALL Java_com_android_client_Cocos_awd(JNIEnv* env, jclass clazz, jint adtype, jstring tag) {
-		CCLOG("receive url result: %d", tag);
 		if (IvySDK::adclosedCallback_ != nullptr) {
 			const char* extra = env->GetStringUTFChars(tag, 0);
 			env->DeleteLocalRef(tag);
@@ -90,7 +82,6 @@ extern "C" {
 	}
 
 	JNIEXPORT void JNICALL Java_com_android_client_Cocos_lar(JNIEnv* env, jclass clazz, jstring tag, jboolean success) {
-		CCLOG("receive url result: %d", tag);
 		if (IvySDK::adloadCallback_ != nullptr) {
 			const char* extra = env->GetStringUTFChars(tag, 0);
 			env->DeleteLocalRef(tag);
@@ -98,16 +89,14 @@ extern "C" {
 		}
 	}
 	JNIEXPORT void JNICALL Java_com_android_client_Cocos_pe(JNIEnv* env, jclass clazz, jstring error) {
-		CCLOG("receive url result: %d", error);
-		if (IvySDK::adloadCallback_ != nullptr) {
+		if (IvySDK::paymentErrorCallback_ != nullptr) {
 			const char* extra = env->GetStringUTFChars(error, 0);
 			env->DeleteLocalRef(error);
 			IvySDK::paymentErrorCallback_(extra);
 		}
 	}
 	JNIEXPORT void JNICALL Java_com_android_client_Cocos_ps(JNIEnv* env, jclass clazz, jstring prices) {
-		CCLOG("receive url result: %d", prices);
-		if (IvySDK::adloadCallback_ != nullptr) {
+		if (IvySDK::receiveBillPricesCallback_ != nullptr) {
 			const char* extra = env->GetStringUTFChars(prices, 0);
 			env->DeleteLocalRef(prices);
 			IvySDK::receiveBillPricesCallback_(extra);
